7ef7203a9ee23ad0cd5de53aa5fe403d49037ea0
Smith <smith.mugalu@andela.com>
2020-11-17 11:11:33 +0300

    Initial Commit

---
+++ Coordinators/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Coordinators
+//
+//  Created by Smithers on 17/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
